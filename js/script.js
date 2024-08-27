48e13b85d42faaad57602d5375b6801aad56a348
kost-ivanou <kost.ivanou@gmail.com>
2024-08-27 18:54:10 +0300

    completed the game

--- js/script.js
+++ js/script.js
@@ -7,28 +7,117 @@ let winComb = [[0, 1, 2],
 [2, 5, 8],
 [2, 4, 6]];
 
+let gameStatus = true;
+
+let turnNumber = 0;
+
 let currentPlayer = "нолик";
 
-const winMsg = `Выиграл ${currentPlayer}`;
+let restartButton = document.querySelector('.game-restart');
+
 const drawMsg = `Ничья`;
-
-
 
 let crossMas = [];
 let zeroMas = [];
 
-const cells = document.querySelector('.game-container').querySelectorAll('.cells');
+const game = document.querySelector('.game-container');
+let cells = game.querySelectorAll('.cell');
+
+restartButton.addEventListener('click', restartGame);
+
+let counter = 0;
+for(let cell of cells){
+    cell.addEventListener('click', turnHandler, {once: true});
+    cell.dataset.index = counter;
+    counter++;
+}
 
 function changePlayer (){
-    currentPlayer = "нолик" ? "крестик" : "нолик";
+    currentPlayer === "нолик" ? currentPlayer = "крестик" : currentPlayer = "нолик";
+    document.querySelector('.game-status').innerHTML = `Ходит ${currentPlayer}`;
 }
 
 function addImage(cell){
     let image = document.createElement('img');
-    currentPlayer = "нолик" ? image.innerHTML = '<img src = "zero.jpg"/>': image.innerHTML = '<img src = "cross.jpg"/>';
-    cell.append(image); 
+    if(currentPlayer === "нолик"){
+        image.src = "img/zero.png";
+    }
+    else{
+        image.src = "img/cross.png";
+    } 
+    cell.appendChild(image); 
 }
 
+function turnHandler(){
+    turnNumber++;
+    currentPlayer === "нолик" ? zeroMas.push(Number(this.dataset.index)) : crossMas.push(Number(this.dataset.index)); 
+    checkWin();
+    addImage(this);
+    if(gameStatus){
+    changePlayer();
+    }
+}
+
+function checkWin(){
+    let crossWin = false;
+    let zeroWin = false;
+    zeroMas.sort();
+    crossMas.sort();
+
+    for(let comb of winComb){
+        if (isSubset(comb, zeroMas)){
+            zeroWin = true;
+        }
+        if (isSubset(comb, crossMas)){
+            crossWin = true;
+        }
+    }
+
+    if(crossWin){
+        document.querySelector('.game-status').innerHTML = "Выиграл крестик!";
+        gameStatus = false;
+        cells.forEach(cell => {
+            cell.removeEventListener('click', turnHandler)
+        }); 
+    }
+
+    else if(zeroWin){
+        document.querySelector('.game-status').innerHTML = "Выиграл нолик!";
+        gameStatus = false;
+        cells.forEach(cell => {
+            cell.removeEventListener('click', turnHandler)
+        }); 
+    }
+
+    else{
+        document.querySelector('.game-status').innerHTML = drawMsg;
+        if(turnNumber == 9){
+            gameStatus = false;
+        }
+    }
+}
+
+function restartGame(){
+    cells.forEach(cell => {
+        cell.innerHTML = '';
+    });
+
+    cells.forEach(cell => {
+        cell.addEventListener('click', turnHandler, {once: true});
+    });
+
+    document.querySelector('.game-status').innerHTML = `Ходит нолик`;
+    currentPlayer = "нолик";
+    zeroMas = [];
+    crossMas = [];
+    gameStatus = true;
+    turnNumber = 0;
+}
+
+function isSubset(arr1, arr2) {
+    const set2 = new Set(arr2);
+    return arr1.every(elem => set2.has(elem));
+}
 
 
 
