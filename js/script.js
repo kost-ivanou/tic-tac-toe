b44eeb5475bb14b4b411552b303b8fc4309c35a0
vlad_haiduk <lolly1bomb2@gmail.com>
2024-08-30 23:07:16 +0300

    finish game

--- js/script.js
+++ js/script.js
@@ -13,95 +13,129 @@ let gameStatus = true;
 let turnNumber = 0;
 let currentPlayer = "нолик";
 let restartButton = document.querySelector('.game-restart');
+const game = document.querySelector('.game-container');
 let cells = game.querySelectorAll('.cell');
 let crossMas = [];
 let zeroMas = [];
 const drawMsg = `Ничья`;
-const game = document.querySelector('.game-container');
 
 restartButton.addEventListener('click', restartGame);
 
 let counter = 0;
-
-for(let cell of cells){
-    cell.addEventListener('click', turnHandler, {once: true});
+for (let cell of cells) {
+    cell.addEventListener('click', turnHandler, { once: true });
     cell.dataset.index = counter;
     counter++;
 }
 
-function changePlayer (){
+function changePlayer() {
     currentPlayer === "нолик" ? currentPlayer = "крестик" : currentPlayer = "нолик";
     document.querySelector('.game-status').innerHTML = `Ходит ${currentPlayer}`;
 }
 
-function addImage(cell){
+function addImage(cell) {
     let image = document.createElement('img');
-    if(currentPlayer === "нолик"){
+    if (currentPlayer === "нолик") {
         image.src = "img/zero.png";
-    }
-    else{
+    } else {
         image.src = "img/cross.png";
     }
     cell.appendChild(image);
 }
 
-function turnHandler(){
+function turnHandler() {
     turnNumber++;
-    currentPlayer === "нолик" ? zeroMas.push(Number(this.dataset.index)) : crossMas.push(Number(this.dataset.index)); 
+    currentPlayer === "нолик" ? zeroMas.push(Number(this.dataset.index)) : crossMas.push(Number(this.dataset.index));
     checkWin();
     addImage(this);
-    if(gameStatus){
-    changePlayer();
+    if (gameStatus) {
+        changePlayer();
     }
 }
 
-function checkWin(){
+function checkWin() {
     let crossWin = false;
     let zeroWin = false;
     zeroMas.sort();
     crossMas.sort();
 
-    for(let comb of winComb){
-        if (isSubset(comb, zeroMas)){
+    for (let comb of winComb) {
+        if (isSubset(comb, zeroMas)) {
             zeroWin = true;
+            drawWinningLine(comb);
+            break;
         }
-        if (isSubset(comb, crossMas)){
+        if (isSubset(comb, crossMas)) {
             crossWin = true;
+            drawWinningLine(comb);
+            break;
         }
     }
 
-    if(crossWin){
+    if (crossWin) {
         document.querySelector('.game-status').innerHTML = "Выиграл крестик!";
         gameStatus = false;
         cells.forEach(cell => {
             cell.removeEventListener('click', turnHandler)
         });
-    }
-
-    else if(zeroWin){
+    } else if (zeroWin) {
         document.querySelector('.game-status').innerHTML = "Выиграл нолик!";
         gameStatus = false;
         cells.forEach(cell => {
             cell.removeEventListener('click', turnHandler)
         });
-    }
-
-    else{
+    } else if (turnNumber === 9) {
         document.querySelector('.game-status').innerHTML = drawMsg;
-        if(turnNumber == 9){
-            gameStatus = false;
-        }
+        gameStatus = false;
     }
 }
 
-function restartGame(){
+function drawWinningLine(comb) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+
+    const cellRect = cells[comb[0]].getBoundingClientRect();
+    const gameRect = game.getBoundingClientRect();
+
+    if (comb[0] % 3 === comb[1] % 3 && comb[1] % 3 === comb[2] % 3) {
+        // Вертикальная линия
+        line.classList.add('vertical');
+        line.style.left = `${cellRect.left - gameRect.left + cellRect.width / 2 - 3}px`;
+        line.style.top = `0px`;
+    } else if (Math.floor(comb[0] / 3) === Math.floor(comb[1] / 3) && Math.floor(comb[1] / 3) === Math.floor(comb[2] / 3)) {
+        // Горизонтальная линия
+        line.classList.add('horizontal');
+        line.style.top = `${cellRect.top - gameRect.top + cellRect.height / 2 - 3}px`;
+        line.style.left = `0px`;
+    } else if (comb[0] === 0 && comb[1] === 4 && comb[2] === 8) {
+        // Диагональная линия (с 0  на 8)
+        line.classList.add('diagonal');
+        line.style.left = `-20%`;
+        line.style.top = `calc(50% - 3px)`;
+    } else if (comb[0] === 2 && comb[1] === 4 && comb[2] === 6) {
+        // Диагональная линия (с 2 на 6)
+        line.classList.add('diagonal-reverse');
+        line.style.left = `-20%`;
+        line.style.top = `calc(50% - 3px)`;
+    }
+
+    game.appendChild(line);
+}
+
+function restartGame() {
     cells.forEach(cell => {
         cell.innerHTML = '';
     });
 
     cells.forEach(cell => {
-        cell.addEventListener('click', turnHandler, {once: true});
+        cell.addEventListener('click', turnHandler, { once: true });
     });
+
+    // Удаление линии
+    const line = document.querySelector('.line');
+    if (line) {
+        line.remove();
+    }
 
     document.querySelector('.game-status').innerHTML = `Ходит нолик`;
     currentPlayer = "нолик";
